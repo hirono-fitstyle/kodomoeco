86af3ebaec2e27613dec706b5ef051ee56dd7036
Ryuichi Tamura <tamura.ryuichi@rail-c.com>
2024-01-08 04:40:44 +0900

    既存アカウントが存在していた場合の初期登録ロジックを追加

--- app/Http/Controllers/Auth/RegisterAccountController.php
+++ app/Http/Controllers/Auth/RegisterAccountController.php
@@ -18,6 +18,12 @@ use App\Models\Operator;
 use Exception;
 use Carbon\Carbon;
 
+class RegisterParams {
+    public string $operator_number;
+    public int $operator_id;
+    public string $operator_passcode;
+}
+
 class RegisterAccountController extends Controller
 {
 
@@ -28,6 +34,12 @@ class RegisterAccountController extends Controller
 
     public function entry(AccountConfirmRequest $request)
     {
+        $obj_account = DB::table('accounts')->where('email', '=', $request->email);
+        
+        if ($obj_account->exists()) {
+            return redirect('entry')->withInput()->with('errors', ['既に登録済みのメールアドレスです。']);
+        }
+
         $request->session()->put('form_inputs', [
             'last_name' => $request->last_name,
             'first_name' => $request->first_name,
@@ -41,9 +53,6 @@ class RegisterAccountController extends Controller
     {
         //セッションから値を取り出す
         $input = $request->session()->pull("form_inputs");
-
-        Log::info('confirm');
-        Log::info($input);
 
         return view('auth.confirm', ["input" => $input]);
 
@@ -77,7 +86,7 @@ class RegisterAccountController extends Controller
             ]);
             $account->save();
 
-            $verification_url = 'http://localhost:8080/entry/completion/' . $verify_token;
+            $verification_url = env('APP_URL') . '/entry/completion/' . $verify_token;
 
             Mail::to($request->email)->send(new VerifyEmailAddressMail($verification_url));
             
@@ -94,13 +103,13 @@ class RegisterAccountController extends Controller
 
     public function completion(Request $request)
     {
-        if ($request->has('token')) {
+        if (!$request->has('token')) {
             // 404エラー
         }
         $token = $request->token;
 
-        Log::info('token');
-        Log::info($token);
+        Log::info('request');
+        Log::info($request->all());
 
         $obj_account = DB::table('accounts')
         ->where('verify_token', '=', $token)
@@ -116,46 +125,48 @@ class RegisterAccountController extends Controller
 
         DB::beginTransaction();
         try {
-            // 事業者番号に使用する数値を取得
-            $operator_number_numlic = $this->getNewOperatorNumber();
+            $previous_operator = Operator::where('staff_mail', '=', $account->email)->first();
+
+            $register_params = $this->getRegisterOperatorParams($previous_operator);
 
             // 初期パスワード生成
             $initial_password = $this->createPassword();
 
-            // 初期パスコード生成
-            $operator_passcode = $this->createPassword('alpha_numlic', 6);
-
-            // 事業者番号
-            $operator_number = 'S' . sprintf('%06d', $operator_number_numlic);
-
-            // 事業者ID
-            $operator_id = '6' . sprintf('%08d', $operator_number_numlic);
-
             // アカウントテーブルに認証情報を保存
             Account::where('verify_token', '=', $token)->update([
-                'operator_number' => $operator_number,
+                'operator_number' => $register_params->operator_number,
                 'email_verified_at' => Carbon::now(),
                 'is_reset_password' => true,
                 'password' => Hash::make($initial_password),
             ]);
 
-            // 事業者情報テーブルに初期値を登録
-            $mst_operator = new Operator();
-            $mst_operator->fill([
-                'operator_number' => $operator_number,
-                'operator_passcode' => $operator_passcode,
-                'operator_id' => $operator_id,
-                'operator_status' => '1',
-                'staff_last_name' => $account->last_name,
-                'staff_first_name' => $account->first_name,
-                'staff_mail' => $account->email,
-            ]);
-            $mst_operator->save();
+            if (is_null($previous_operator)) {
+                // 事業者情報テーブルに初期値を登録
+                $mst_operator = new Operator();
+                $mst_operator->fill([
+                    'operator_number' => $register_params->operator_number,
+                    'operator_passcode' => $register_params->operator_passcode,
+                    'operator_id' => $register_params->operator_id,
+                    'operator_status' => '1',
+                    'staff_last_name' => $account->last_name,
+                    'staff_first_name' => $account->first_name,
+                    'staff_mail' => $account->email,
+                ]);
+                $mst_operator->save();
+            } else {
+                // 既存データを更新
+                Operator::where('operator_number', '=', $register_params->operator_number)->update([
+                    'operator_passcode' => $register_params->operator_passcode,
+                    'operator_status' => '1',
+                    'staff_last_name' => $account->last_name,
+                    'staff_first_name' => $account->first_name,
+                ]);
+            }
 
             $staff_name = $account->last_name . ' ' . $account->first_name;
 
             Mail::to($account->email)
-            ->send(new AccountIssuanceCompleted($staff_name, $operator_id, $initial_password));
+            ->send(new AccountIssuanceCompleted($staff_name, $register_params->operator_id, $initial_password));
             
             DB::commit();
         } catch (Exception $e) {
@@ -207,6 +218,26 @@ class RegisterAccountController extends Controller
         } else {
             return (int) substr($max_operator_number, 1) + 1;
         }
+    }
+
+    // 初期登録用のパラメータを取得
+    private function getRegisterOperatorParams(Operator $mst_previous_operator = null)
+    {
+        $params = new RegisterParams();
+        if (is_null($mst_previous_operator)) {
+            // 事業者番号に使用する数値を取得
+            $operator_number_numlic = $this->getNewOperatorNumber();
+            $params->operator_number = 'S' . sprintf('%06d', $operator_number_numlic);
+            $params->operator_id = intval('6' . sprintf('%08d', $operator_number_numlic));
+            $params->operator_passcode = $this->createPassword('', 6);
+        } else {
+            $operator = $mst_previous_operator->first();
+            $params->operator_number = $operator->operator_number;
+            $params->operator_id = $operator->operator_id;
+            $params->operator_passcode = $operator->operator_passcode;
+        }
+
+        return $params;
     }
 
 }
