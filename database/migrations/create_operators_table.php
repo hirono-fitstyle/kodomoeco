7aa1d7e6c20d2d8fc14358f17e08242e8d42f76f
hirono-fitstyle <k.hirono@fitstyle-tt.co.jp>
2024-01-08 12:51:18 +0900

    事業者情報マイグレーションファイル 2024年度版対応

--- database/migrations/create_operators_table.php
+++ database/migrations/create_operators_table.php
@@ -43,9 +43,10 @@ return new class extends Migration
             $table->string('staff_tel', '11')->nullable()->comment('担当者 電話番号（固定）');
             $table->string('staff_mobile', '11')->nullable()->comment('担当者 電話番号（携帯）');
             $table->string('staff_mail', '100')->comment('担当者 メールアドレス');
-            $table->boolean('public_project1')->nullable()->comment('公開情報 参加補助金事業（こどもエコすまい）');
-            $table->boolean('public_project2')->nullable()->comment('公開情報 参加補助金事業（高効率給湯器）');
-            $table->boolean('public_project3')->nullable()->comment('公開情報 参加補助金事業（断熱窓）');
+            $table->boolean('public_project1')->nullable()->comment('公開情報 参加補助金事業（子育てエコホーム）');
+            $table->boolean('public_project2')->nullable()->comment('公開情報 参加補助金事業（給湯省エネ）');
+            $table->boolean('public_project3')->nullable()->comment('公開情報 参加補助金事業（先進的窓リノベ）');
+            $table->boolean('public_project3')->nullable()->comment('公開情報 参加補助金事業（賃貸集合給湯省エネ）');
             $table->boolean('public_buisiness1')->nullable()->comment('公開情報 参加事業（注文住宅の新築）');
             $table->boolean('public_buisiness2')->nullable()->comment('公開情報 参加事業（新築分譲住宅の販売）');
             $table->boolean('public_buisiness3')->nullable()->comment('公開情報 参加事業（リフォーム工事）');
@@ -63,9 +64,10 @@ return new class extends Migration
             $table->boolean('reform_flag')->nullable()->comment('住宅リフォーム事業者団体登録有無');
             $table->string('reform_association', '2')->nullable()->comment('住宅リフォーム事業者団体');
             $table->string('reform_association_url', '100')->nullable()->comment('住宅リフォーム事業者団体URL');
-            $table->boolean('public_project_abolish_flag1')->nullable()->comment('公開情報 参加補助金事業（こどもエコすまい）廃止フラグ');
-            $table->boolean('public_project_abolish_flag2')->nullable()->comment('公開情報 参加補助金事業（高効率給湯器）廃止フラグ');
-            $table->boolean('public_project_abolish_flag3')->nullable()->comment('公開情報 参加補助金事業（断熱窓）廃止フラグ');
+            $table->boolean('public_project_abolish_flag1')->nullable()->comment('公開情報 参加補助金事業（子育てエコホーム）廃止フラグ');
+            $table->boolean('public_project_abolish_flag2')->nullable()->comment('公開情報 参加補助金事業（給湯省エネ）廃止フラグ');
+            $table->boolean('public_project_abolish_flag3')->nullable()->comment('公開情報 参加補助金事業（先進的窓リノベ）廃止フラグ');
+            $table->boolean('public_project_abolish_flag3')->nullable()->comment('公開情報 参加補助金事業（賃貸集合給湯省エネ）廃止フラグ');
             $table->boolean('recieve_mail_flag')->nullable()->comment('メール受信希望フラグ');
             $table->boolean('privacy_policy_consent1')->nullable()->comment('プライバシーポリシー同意1');
             $table->boolean('privacy_policy_consent2')->nullable()->comment('プライバシーポリシー同意2');
